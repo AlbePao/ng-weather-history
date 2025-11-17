cd708212f71f7c20888b68394579bd008b15eab1
Alberto Paolucci <apaolucci@softstrategy.it>
2025-11-17 18:11:24 +0100

    added and configured ngx-translate

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,14 @@
-import {
-  ApplicationConfig,
-  provideBrowserGlobalErrorListeners,
-  provideZoneChangeDetection,
-} from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { provideTranslateService } from '@ngx-translate/core';
+import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
+    provideTranslateService({
+      loader: provideTranslateHttpLoader({ prefix: './i18n/', suffix: '.json' }),
+      fallbackLang: 'en',
+    }),
   ],
 };
