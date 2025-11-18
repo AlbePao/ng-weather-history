fbbc2d74746067ef0a53a2695ac06073036b158e
Alberto Paolucci <apaolucci@softstrategy.it>
2025-11-18 12:16:30 +0100

    provided missing http client

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,11 @@
+import { provideHttpClient } from '@angular/common/http';
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideTranslateService } from '@ngx-translate/core';
 import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideHttpClient(),
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideTranslateService({
