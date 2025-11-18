93682a2260616f6ae902ba4fbb6b33bfe4bc565e
Alberto Paolucci <apaolucci@softstrategy.it>
2025-11-18 10:37:05 +0100

    input directive

---
+++ src/app/components/input/input.directive.ts
@@ -0,0 +1,99 @@
+import {
+  Directive,
+  DoCheck,
+  ElementRef,
+  EventEmitter,
+  Input,
+  Output,
+  booleanAttribute,
+  effect,
+  inject,
+  input,
+} from '@angular/core';
+import { AbstractControl, NgControl, Validators } from '@angular/forms';
+import { getUniqueId } from '@utils/getUniqueId';
+
+// Invalid input type. Using one of these will throw an MatInputUnsupportedTypeError.
+const APP_INPUT_INVALID_TYPES = ['button', 'checkbox', 'file', 'hidden', 'image', 'radio', 'range', 'reset', 'submit'];
+
+@Directive({
+  selector: 'input[appInput], textarea[appInput], select[appSelect]',
+  host: {
+    '[class]': 'classes',
+    '[id]': 'id',
+    '[attr.disabled]': 'disabled || null',
+    '[attr.required]': 'required || null',
+    '(blur)': 'elementBlur.emit()',
+  },
+})
+export class InputDirective implements DoCheck {
+  private readonly _ngControl = inject(NgControl, { self: true, optional: true });
+  private readonly _elementRef =
+    inject<ElementRef<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>>(ElementRef);
+
+  readonly type = input('text');
+
+  @Input() id = getUniqueId('app-input');
+
+  @Input({ transform: booleanAttribute }) disabled = false;
+
+  @Input({ transform: booleanAttribute })
+  get required(): boolean {
+    return (this._required || this.control?.hasValidator(Validators.required)) ?? false;
+  }
+  set required(value: boolean) {
+    this._required = value;
+  }
+  protected _required = false;
+
+  @Output() readonly elementBlur = new EventEmitter<void>();
+
+  get classes(): string {
+    const borderColorClasses = this.invalid
+      ? 'border-danger focus:border-danger focus:ring-danger/40 '
+      : `border-gray-darker focus:border-primary focus:ring-primary/40`;
+
+    const paddingClasses = this._isSelect ? 'pl-2.5 pr-8' : 'px-2.5';
+
+    return `block min-h-10 ${paddingClasses} w-full text-sm text-black rounded-sm border appearance-none focus:ring-4 focus:ring-offset-0 peer select-none disabled:bg-gray-lighter disabled:opacity-50 ${borderColorClasses}`;
+  }
+
+  get hostElement(): HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement {
+    return this._elementRef.nativeElement;
+  }
+
+  get invalid(): boolean {
+    return !!(this.control?.dirty ?? this.control?.touched) && !!this.control?.invalid;
+  }
+
+  get control(): AbstractControl<unknown, unknown> | null {
+    return this._ngControl?.control ?? null;
+  }
+
+  get nodeName(): string {
+    return this.hostElement.nodeName.toLowerCase();
+  }
+
+  private readonly _isInput = this.nodeName === 'input';
+  private readonly _isSelect = this.nodeName === 'select';
+
+  constructor() {
+    effect(() => {
+      if (this.type() && this._isInput) {
+        const type = this.type();
+
+        if (APP_INPUT_INVALID_TYPES.includes(type)) {
+          throw new Error(`InputDirective: Input type "${type}" isn't supported`);
+        }
+
+        this._elementRef.nativeElement.setAttribute('type', type);
+      }
+    });
+  }
+
+  ngDoCheck(): void {
+    if (this._ngControl && this._ngControl.disabled !== null && this._ngControl.disabled !== this.disabled) {
+      this.disabled = this._ngControl.disabled;
+    }
+  }
+}
---
+++ src/app/components/input/input.md
@@ -0,0 +1,90 @@
+# Input
+
+`appInput` is a directive that allows native `<input>` and `<textarea>` elements to work with `<app-form-field>`.
+
+## Inputs
+
+| Input      | Description                          | Type      |
+| ---------- | ------------------------------------ | --------- |
+| `id`       | (optional) Unique ID for the input   | `string`  |
+| `disabled` | Whether the input is disabled or not | `boolean` |
+
+## Outputs
+
+| Output         | Description                                   | Type emitted |
+| -------------- | --------------------------------------------- | ------------ |
+| `elementFocus` | Event emitted when the component gets focused | `void`       |
+| `elementBlur`  | Event emitted when the component loses focus  | `void`       |
+
+## Example
+
+### Reactive Form
+
+```typescript
+// example.component.ts
+@Component({
+  // other stuff...
+  imports: [
+    // other imports...
+    FormFieldModule,
+    InputDirective,
+    // other imports...
+  ],
+  // other stuff...
+})
+export class ExampleComponent implements OnInit {
+  private readonly _fb = inject(FormBuilder);
+
+  form = this._fb.group({
+    inputControlExample: [''],
+  });
+
+  ngOnInit(): void {
+    this.form.valueChanges.subscribe((value) => {
+      console.log('form values', value);
+    });
+  }
+}
+```
+
+```html
+<!-- example.component.html -->
+<form [formGroup]="form">
+  <app-form-field>
+    <app-label>Input Control Example</app-label>
+    <input appInput type="text" formControlName="inputControlExample" />
+    @if (form.controls.inputControlExample | showControlError: 'required') {
+    <app-error>Required field</app-error>
+    }
+  </app-form-field>
+</form>
+```
+
+### Standalone
+
+```typescript
+// example.component.ts
+@Component({
+  // other stuff...
+  imports: [
+    // other imports...
+    FormFieldModule,
+    InputDirective,
+    // other imports...
+  ],
+  // other stuff...
+})
+export class ExampleComponent {
+  logInputChange(event: Event): void {
+    console.log('logInputChange', event);
+  }
+}
+```
+
+```html
+<!-- example.component.html -->
+<app-form-field>
+  <app-label>Input Example</app-label>
+  <input appInput type="text" (change)="logInputChange($event)" />
+</app-form-field>
+```
