b9da1b6150feb6e37ac7c126263e25176df85f11
Alberto Paolucci <apaolucci@softstrategy.it>
2025-11-24 17:33:57 +0100

    ChartWindSpeedHistoryComponent

---
+++ src/app/components/chart-wind-speed-history/chart-wind-speed-history.component.ts
@@ -0,0 +1,49 @@
+import { ChangeDetectionStrategy, Component, computed } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ChartHistoryBaseComponent } from '@components/chart-history-base';
+import { RadioOptionsComponent } from '@components/radio-options';
+import { setDataByTimeRange } from '@utils/groupDataByTimeRange';
+import { HighchartsChartComponent } from 'highcharts-angular';
+
+@Component({
+  selector: 'app-chart-wind-speed-history',
+  imports: [HighchartsChartComponent, RadioOptionsComponent, FormsModule],
+  templateUrl: './chart-wind-speed-history.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ChartWindSpeedHistoryComponent extends ChartHistoryBaseComponent {
+  override chartOptions = computed<Highcharts.Options>(() => {
+    const weatherData = this.weatherData();
+    const units = this.units();
+
+    if (!weatherData || !units) {
+      return { title: { text: 'Wind Speed History' } };
+    }
+
+    const granularity = this.timeGranularity();
+    const { time, wind_speed_10m_mean: meanWindSpeed } = weatherData;
+
+    const { time: formattedTime, data: meanWindSpeedData } = setDataByTimeRange(granularity, time, meanWindSpeed);
+
+    return {
+      title: { text: 'Wind Speed History' },
+      xAxis: {
+        title: { text: 'Date' },
+        labels: { rotation: -45 },
+        categories: formattedTime,
+      },
+      yAxis: {
+        title: { text: `Wind Speed (${units.wind_speed_10m_mean})` },
+      },
+      series: [
+        {
+          // Wind speed data
+          color: 'green',
+          data: meanWindSpeedData,
+          type: 'line',
+          showInLegend: false,
+        },
+      ],
+    };
+  });
+}
