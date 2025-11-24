9da62c55d30311383f35c97e98b54bca2e1020ab
Alberto Paolucci <apaolucci@softstrategy.it>
2025-11-24 17:33:04 +0100

    ChartHumidityHistoryComponent

---
+++ src/app/components/chart-humidity-history/chart-humidity-history.component.ts
@@ -0,0 +1,49 @@
+import { ChangeDetectionStrategy, Component, computed } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ChartHistoryBaseComponent } from '@components/chart-history-base';
+import { RadioOptionsComponent } from '@components/radio-options';
+import { setDataByTimeRange } from '@utils/groupDataByTimeRange';
+import { HighchartsChartComponent } from 'highcharts-angular';
+
+@Component({
+  selector: 'app-chart-humidity-history',
+  imports: [HighchartsChartComponent, RadioOptionsComponent, FormsModule],
+  templateUrl: './chart-humidity-history.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ChartHumidityHistoryComponent extends ChartHistoryBaseComponent {
+  override chartOptions = computed<Highcharts.Options>(() => {
+    const weatherData = this.weatherData();
+    const units = this.units();
+
+    if (!weatherData || !units) {
+      return { title: { text: 'Humidity History' } };
+    }
+
+    const granularity = this.timeGranularity();
+    const { time, relative_humidity_2m_mean: meanHumidity } = weatherData;
+
+    const { time: formattedTime, data: meanHumidityData } = setDataByTimeRange(granularity, time, meanHumidity);
+
+    return {
+      title: { text: 'Humidity History' },
+      xAxis: {
+        title: { text: 'Date' },
+        labels: { rotation: -45 },
+        categories: formattedTime,
+      },
+      yAxis: {
+        title: { text: `Humidity (${units.relative_humidity_2m_mean})` },
+      },
+      series: [
+        {
+          // Humidity data
+          color: 'oklch(60.9% 0.126 221.723)',
+          data: meanHumidityData,
+          type: 'line',
+          showInLegend: false,
+        },
+      ],
+    };
+  });
+}
