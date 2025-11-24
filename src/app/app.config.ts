be1a6a37f7e020319cbc16962e88c089f395efcd
Alberto Paolucci <apaolucci@softstrategy.it>
2025-11-24 15:35:06 +0100

    added highcharts and highcharts-angular

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,12 @@
 import { provideHttpClient } from '@angular/common/http';
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { provideHighcharts } from 'highcharts-angular';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideHttpClient(),
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
+    provideHighcharts(),
   ],
 };
